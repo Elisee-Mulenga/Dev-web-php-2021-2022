17517b2d05a2be9f938c2f66eba1b088f7b4884f
MULENGA MULAPU Elisée <mulengaelisee2@gmail.com>
2022-01-19 22:00:39 +0100

    Premier commit

---
+++ sc.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>Elisee - Php cours</title>
+    </head>
+
+    <body>
+        <?php
+        echo "Hello world, ESIS une ecole un metier !";
+        ?>
+
+        <?php
+          $couple = ['' ] 
+        ?>
+
+        <?php
+            // On crée notre array $coordonnees
+            $coordonnees = array (
+            'prenom' => 'François',
+            'nom' => 'Dupont',
+            'adresse' => '3 Rue du Paradis',
+            'ville' => 'Marseille');
+            echo 'ville';
+        ?>
+
+    </body>
+</html>
+
