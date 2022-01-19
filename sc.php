14a99a8ac77693883ab8f014905f4c61695e7bf9
MULENGA MULAPU Elisée <mulengaelisee2@gmail.com>
2022-01-19 22:08:03 +0100

    modification du code

--- sc.php
+++ sc.php
@@ -9,16 +9,12 @@
         ?>
 
         <?php
-          $couple = ['' ] 
-        ?>
-
-        <?php
             // On crée notre array $coordonnees
             $coordonnees = array (
             'prenom' => 'François',
             'nom' => 'Dupont',
             'adresse' => '3 Rue du Paradis',
-            'ville' => 'Marseille');
+            'ville' => 'Paris ma ville adorée');
             echo 'ville';
         ?>
 
